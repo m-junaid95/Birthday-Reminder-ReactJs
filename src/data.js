921b59cefd571d03f31168baf5ed9d009dc6242f
Junaid <zulfiqar.junaid95@gmail.com>
2022-03-30 17:34:12 -0700

    First Commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Olivia',
+    age: 26,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Isla Hogan',
+    age: 35,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'William Lorry',
+    age: 22,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Jack Thomas',
+    age: 33,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Willow Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
